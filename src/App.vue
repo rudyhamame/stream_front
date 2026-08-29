463cf4b89d3aff6714a408c3ec2b35b7d96a5585
Rudy Hamame <77045003+rudyhamame@users.noreply.github.com>
2026-08-29 04:25:42 -0400

    Remove media type labels from library cards

--- src/App.vue
+++ src/App.vue
@@ -952,7 +952,7 @@ onMounted(async () => {
           <section v-for="rail in libraryRails" :key="rail.name" class="safari-library-rail">
             <header><h2>{{ rail.name }}</h2><span>{{ rail.items.length }}</span></header>
             <div class="safari-library-rail-track" :class="{'is-scrolling-left': safariRailMotion[rail.name] === 'left', 'is-scrolling-right': safariRailMotion[rail.name] === 'right'}" @scroll="handleSafariRailScroll($event, rail.name)">
-              <button v-for="item in rail.items" :key="item.key" type="button" :class="{'is-playable': safariLibraryTab === 'movie'}" @click="safariLibraryTab === 'movie' && playWebMovie(item)"><span class="safari-library-art"><img v-if="item.logo" :src="imageUrl(item.logo)" :alt="item.title"><template v-else><img class="safari-library-fallback" src="/home-background.png" alt=""><b>{{ typeIcon(safariLibraryTab) }}</b></template></span><span><strong>{{ item.title }}</strong><small>{{ typeLabel(safariLibraryTab) }}</small></span><em v-if="safariLibraryTab === 'movie'">Play</em></button>
+              <button v-for="item in rail.items" :key="item.key" type="button" :class="{'is-playable': safariLibraryTab === 'movie'}" @click="safariLibraryTab === 'movie' && playWebMovie(item)"><span class="safari-library-art"><img v-if="item.logo" :src="imageUrl(item.logo)" :alt="item.title"><template v-else><img class="safari-library-fallback" src="/home-background.png" alt=""><b>{{ typeIcon(safariLibraryTab) }}</b></template></span><span><strong>{{ item.title }}</strong></span><em v-if="safariLibraryTab === 'movie'">Play</em></button>
             </div>
           </section>
         </div>
