7d31e4eb4ace468ce8103be4a9416431c1ea31a3
Rudy Hamame <77045003+rudyhamame@users.noreply.github.com>
2026-08-29 04:30:58 -0400

    Move fullscreen control into player

---
+++ src/components/icons/MaximizeIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="24" :height="24" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+    <path d="m15.71 14.29-1.42 1.42 3 3L15 21h6v-6l-2.29 2.29zM8.29 9.71l1.42-1.42-3-3L9 3H3v6l2.29-2.29zm9-4.42-3 3 1.42 1.42 3-3L21 9V3h-6zM6.71 18.71l3-3-1.42-1.42-3 3L3 15v6h6z" />
+  </svg>
+</template>
+
+<script setup>
+defineOptions({ name: "MaximizeIcon" });
+</script>
