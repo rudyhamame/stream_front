083192932f1ffaf7bbc90cd7b053552c5ae92c74
Rudy Hamame <77045003+rudyhamame@users.noreply.github.com>
2026-09-02 13:04:08 -0400

    sdf

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import os from 'node:os';
 
 const browserProxy = {
   '/api/xtream/hls': { target: 'http://127.0.0.1:8788', changeOrigin: true },
   '/api/xtream/play': { target: 'http://127.0.0.1:8788', changeOrigin: true },
   '/api': { target: 'http://127.0.0.1:8787', changeOrigin: true }
 };
-const allowedHosts = ['rudy-hp-pavilion-x360-convertible-14-ba0xx.tailb5a10d.ts.net'];
+const networkHosts = Object.values(os.networkInterfaces())
+  .flatMap((interfaces) => interfaces || [])
+  .filter((address) => address.family === 'IPv4' && !address.internal)
+  .map((address) => address.address);
+const allowedHosts = [
+  'rudy-hp-pavilion-x360-convertible-14-ba0xx.tailb5a10d.ts.net',
+  ...networkHosts,
+];
 
 export default defineConfig({
   plugins: [vue()],
